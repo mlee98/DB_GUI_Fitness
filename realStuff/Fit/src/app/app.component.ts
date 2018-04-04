5ae7dff6cc9f5cc40a00d437281db356b5fc6367
Robbie Keehan <Rkeehan@wrls10-8-12-209.resnet.smu.edu>
2018-04-04 12:27:15 -0500

    updating real stuff for class

--- realStuff/Fit/src/app/app.component.ts
+++ realStuff/Fit/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+
+const appRoutes: Routes = [
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'sign-in',      component: SignInComponent },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  public frontPage: boolean;
+
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.frontPage = true;
+  }
+
+  public changePage() {
+    this.frontPage = false;
+  }
+
 }
--- realStuff/Fit/src/app/app.module.ts
+++ realStuff/Fit/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ProfileComponent } from './profile/profile.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
