d4a9e77b6cadcf9932a190652e9094b141fcc912
mlee98 <matthew.lee.98@gmail.com>
2018-04-17 11:57:34 -0500

    Made the switch to connection pool and saved 15% on car ensurance

--- DB_sample_stuff/node/server.js
+++ DB_sample_stuff/node/server.js
@@ -10,7 +10,7 @@ server.connection({ port: 3000, host: '0.0.0.0' });
 //Initialize the mysql variable and create the connection object with necessary values
 //Uses the https://www.npmjs.com/package/mysql package.
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
 
     //host will be the name of the service from the docker-compose file. 
     host     : 'mysql',
@@ -19,8 +19,6 @@ var connection = mysql.createConnection({
     database: 'DB_GUI',
     multipleStatements: true
 });
-
-connection.connect();
 
 server.route({
     method: 'POST',
@@ -40,21 +38,28 @@ server.route({
 
         let uInfoTable = 'INSERT INTO UserInfo (UserId, fName, lName, Height, Weight, Age, UserName)';
         uInfoTable += " VALUES (default, \'" + fName + "\', \'" + lName + "\', \'" + Height + "\', \'" + Weight + "\', \'" + Age + "\',\'" + UserName + "\')";
-        connection.query(uInfoTable, function (error, r1, fields) {
-            if (error)
-                throw error;
-            connection.query(getUserId, function (error, r2, fields) {
+
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(uInfoTable, function (error, r1, fields) {
                 if (error)
                     throw error;
-                let userID = r2[0].UserId;
-                let loginTable = "INSERT INTO Login (UserId, UserName, Password) VALUES(" + userID + ", \'" + UserName + "\', \'" + hashedPW + "\')";
-                connection.query(loginTable, function (error, r3, fields) {
+                connection.query(getUserId, function (error, r2, fields) {
                     if (error)
                         throw error;
-                    reply(userID);
+                    let userID = r2[0].UserId;
+                    let loginTable = "INSERT INTO Login (UserId, UserName, Password) VALUES(" + userID + ", \'" + UserName + "\', \'" + hashedPW + "\')";
+                    connection.query(loginTable, function (error, r3, fields) {
+                        if (error)
+                            throw error;
+                        reply(userID);
+                    });
                 });
             });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -68,19 +73,25 @@ server.route({
 
         let query = "SELECT * FROM Login";
         let loginMessage = '';
-        connection.query(query, function (error, results, fields) {
-            if (error)
-                throw error;
-            for (let i = 0; i < results.length; i++) {
-                if (results[i].UserName === UserName && results[i].Password === hashedPW) {
-                    loginMessage = "You're in!"
-                    break;
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(query, function (error, results, fields) {
+                if (error)
+                    throw error;
+                for (let i = 0; i < results.length; i++) {
+                    if (results[i].UserName === UserName && results[i].Password === hashedPW) {
+                        loginMessage = "You're in!"
+                        break;
+                    }
+                    loginMessage = "Username or Password incorrect. Try again";
                 }
-                loginMessage = "Username or Password incorrect. Try again";
-            }
 
-            reply(loginMessage);
+                reply(loginMessage);
+            });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -89,22 +100,26 @@ server.route({
     method: 'GET',
     path: '/accounts',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM UserInfo', function (error, results, fields) {
-            if (error)
-                throw error;
-            let accNames = '';
-            for (let acc = 0; acc < results.length; acc++) {
-                accNames += results[acc].UserId;
-                accNames += ' ';
-                accNames += results[acc].fName;
-                accNames += ' ';
-                accNames += results[acc].lName;
-                accNames += ' ';
-                accNames += results[acc].Age;
-                accNames += '   ';   
-            }
-            reply(accNames);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM UserInfo', function (error, results, fields) {
+                if (error)
+                    throw error;
+                let accNames = '';
+                for (let acc = 0; acc < results.length; acc++) {
+                    accNames += results[acc].UserId;
+                    accNames += ' ';
+                    accNames += results[acc].fName;
+                    accNames += ' ';
+                    accNames += results[acc].lName;
+                    accNames += ' ';
+                    accNames += results[acc].Age;
+                    accNames += '   ';
+                }
+                reply(accNames);
 
+            });
+            connection.release();//release the connection
         });
     }
 });
@@ -114,27 +129,33 @@ server.route({
     path: '/accounts/profile',
     handler: function (request, reply) {
         let UserId = request.payload['UserId'];
-        connection.query('SELECT * FROM UserInfo WHERE UserId = ' + UserId, function (error, results, fields) {
-            if (error)
-                throw error;
-            let accNames = '';
-            for (let acc = 0; acc < results.length; acc++) {
-                accNames += results[acc].UserId;
-                accNames += ' ';
-                accNames += results[acc].fName;
-                accNames += ' ';
-                accNames += results[acc].lName;
-                accNames += ' ';
-                accNames += results[acc].Height;
-                accNames += ' ';
-                accNames += results[acc].Weight;
-                accNames += ' ';
-                accNames += results[acc].Age;
-                accNames += '   ';
-            }
-            reply(accNames);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM UserInfo WHERE UserId = ' + UserId, function (error, results, fields) {
+                if (error)
+                    throw error;
+                let accNames = '';
+                for (let acc = 0; acc < results.length; acc++) {
+                    accNames += results[acc].UserId;
+                    accNames += ' ';
+                    accNames += results[acc].fName;
+                    accNames += ' ';
+                    accNames += results[acc].lName;
+                    accNames += ' ';
+                    accNames += results[acc].Height;
+                    accNames += ' ';
+                    accNames += results[acc].Weight;
+                    accNames += ' ';
+                    accNames += results[acc].Age;
+                    accNames += '   ';
+                }
+                reply(accNames);
 
+            });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -142,21 +163,27 @@ server.route({
     method: 'GET',
     path: '/accounts/loginInfo',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM Login', function (error, results, fields) {
-            if (error)
-                throw error;
-            let accNames = '';
-            for (let acc = 0; acc < results.length; acc++) {
-                accNames += results[acc].UserId;
-                accNames += ' ';
-                accNames += results[acc].UserName;
-                accNames += ' ';
-                accNames += results[acc].Password;
-                accNames += '   ';
-            }
-            reply(accNames);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM Login', function (error, results, fields) {
+                if (error)
+                    throw error;
+                let accNames = '';
+                for (let acc = 0; acc < results.length; acc++) {
+                    accNames += results[acc].UserId;
+                    accNames += ' ';
+                    accNames += results[acc].UserName;
+                    accNames += ' ';
+                    accNames += results[acc].Password;
+                    accNames += '   ';
+                }
+                reply(accNames);
 
+            });
+
+            connection.release();//release the connection
         });
+        
        
     }
 });
@@ -170,11 +197,17 @@ server.route({
         let Share = request.payload['Share'];
         let query = 'INSERT INTO Account_Allergies (AllergyID, UserId, Name, Share)';
         query += " VALUES (default ,\'" + UserId + "\', \'" + Name + "\', \'" + Share + "\')";
-        connection.query(query, function (error, results, fields) {
-            if (error)
-                throw error;
-            reply("Allergy added: " + query);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(query, function (error, results, fields) {
+                if (error)
+                    throw error;
+                reply("Allergy added: " + query);
+            });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -187,11 +220,17 @@ server.route({
         let Share = request.payload['Share'];
         let query = 'INSERT INTO Account_Sleep (SleepID, UserId, Name, Share)';
         query += " VALUES (default ,\'" + UserId + "\', \'" + Name + "\', \'" + Share + "\')";
-        connection.query(query, function (error, results, fields) {
-            if (error)
-                throw error;
-            reply("Sleep Disorder added: " + query);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(query, function (error, results, fields) {
+                if (error)
+                    throw error;
+                reply("Sleep Disorder added: " + query);
+            });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -204,11 +243,17 @@ server.route({
         let Share = request.payload['Share'];
         let query = 'INSERT INTO Account_Disorders (DisorderID, UserId, Name, Share)';
         query += " VALUES (default ,\'" + UserId + "\', \'" + Name + "\', \'" + Share + "\')";
-        connection.query(query, function (error, results, fields) {
-            if (error)
-                throw error;
-            reply("Eating Disorder added: " + query);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(query, function (error, results, fields) {
+                if (error)
+                    throw error;
+                reply("Eating Disorder added: " + query);
+            });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -222,11 +267,17 @@ server.route({
         let deleteAllergies = 'DELETE FROM Account_Allergies WHERE UserId = ' + UserId + ';';
         let deleteSleep = 'DELETE FROM Account_Sleep WHERE UserId = ' + UserId + ';';
         let deleteLogin = 'DELETE FROM Login WHERE UserId = ' + UserId;
-        connection.query(deleteAccount + deleteDisorders + deleteAllergies + deleteSleep + deleteLogin, function (error, results, fields) {
-            if (error)
-                throw error;
-            reply('User Deleted');
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(deleteAccount + deleteDisorders + deleteAllergies + deleteSleep + deleteLogin, function (error, results, fields) {
+                if (error)
+                    throw error;
+                reply('User Deleted');
+            });
+
+            connection.release();//release the connection
         });
+        
     }
 });
 
@@ -251,44 +302,43 @@ server.route({
             }
         }
         let query = 'SELECT UserName FROM ' + table + ' NATURAL JOIN UserInfo WHERE Name = "' + condition + '" AND UserId != ' + UserId + ' AND Share = 1';
-        connection.query(query, function (error, users, fields) {
-            if (error)
-                throw error;
-            let usernames = '';
-            for (let i = 0; i < users.length; i++) {
-                usernames += (' ' + users[i].UserName);
-            }
-            reply(usernames);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query(query, function (error, users, fields) {
+                if (error)
+                    throw error;
+                let usernames = '';
+                for (let i = 0; i < users.length; i++) {
+                    usernames += (' ' + users[i].UserName);
+                }
+                reply(usernames);
+            });
+
+            connection.release();//release the connection
         });
+        
         
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/test',
-    handler: function (request, reply) {
-        
-        let query2 = 'SELECT * FROM Account_Allergies NATURAL JOIN UserInfo WHERE Name = "Citrus" AND UserId != 15';
-        connection.query(query2, function (error, users, fields) {
-            if (error)
-                throw error;
-            reply(users);
-        });
-    }
-});
 
 
 server.route({
     method: 'GET',
     path: '/accounts/allergies',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM Account_Allergies', function (error, results, fields) {
-            if (error)
-                throw error;
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM Account_Allergies', function (error, results, fields) {
+                if (error)
+                    throw error;
 
-            reply(results);
+                reply(results);
+            });
+
+            connection.release();//release the connection
         });
+        
         //close the connection to MySQL
     }
 });
@@ -297,12 +347,18 @@ server.route({
     method: 'GET',
     path: '/accounts/eatDisorders',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM Account_Disorders', function (error, results, fields) {
-            if (error)
-                throw error;
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM Account_Disorders', function (error, results, fields) {
+                if (error)
+                    throw error;
 
-            reply(results);
+                reply(results);
+            });
+
+            connection.release();//release the connection
         });
+        
         //close the connection to MySQL
     }
 });
@@ -311,12 +367,18 @@ server.route({
     method: 'GET',
     path: '/accounts/sleepDisorders',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM Account_Sleep', function (error, results, fields) {
-            if (error)
-                throw error;
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM Account_Sleep', function (error, results, fields) {
+                if (error)
+                    throw error;
 
-            reply(results);
+                reply(results);
+            });
+
+            connection.release();//release the connection
         });
+        
         //close the connection to MySQL
     }
 });
@@ -327,12 +389,17 @@ server.route({
     method: 'GET',
     path: '/ideal',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM Ideal_body', function (error, results, fields) {
-            if (error)
-                throw error;
-            
-            reply(results);
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM Ideal_body', function (error, results, fields) {
+                if (error)
+                    throw error;
+
+                reply(results);
+            });
+            connection.release();//release the connection
         });
+        
         //close the connection to MySQL
     }
 });
@@ -343,12 +410,18 @@ server.route({
     method: 'GET',
     path: '/sleep/insomnia',
     handler: function (request, reply) {
-        connection.query('SELECT description FROM Sleep WHERE name = "insomnia"', function (error, results, fields) {
-            if (error)
-                throw error;
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT description FROM Sleep WHERE name = "insomnia"', function (error, results, fields) {
+                if (error)
+                    throw error;
 
-            reply(results);
+                reply(results);
+            });
+
+            connection.release();//release the connection
         });
+        
         //close the connection to MySQL
     }
 });
@@ -357,12 +430,18 @@ server.route({
     method: 'GET',
     path: '/sleep',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM Sleep', function (error, results, fields) {
-            if (error)
-                throw error;
+        connection.getConnection(function (err, connection) {
+            //run the query
+            connection.query('SELECT * FROM Sleep', function (error, results, fields) {
+                if (error)
+                    throw error;
 
-            reply(results);
+                reply(results);
+            });
+
+            connection.release();//release the connection
         });
+        
         //close the connection to MySQL
     }
 });
