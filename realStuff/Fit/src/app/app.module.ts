d5b8909c80d329dbbed48c087b12054e45a27742
Noah Rothchild <rothchild.noah@gmail.com>
2018-04-18 17:57:45 -0500

    lab

--- realStuff/Fit/src/app/app.module.ts
+++ realStuff/Fit/src/app/app.module.ts
@@ -6,12 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AccountModule } from './accounts/account.module';
-import { FoodTodayComponent } from './food-today/food-today.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FoodTodayComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
