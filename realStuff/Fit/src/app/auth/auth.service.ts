ec20519b320d7bbdc0923eb1fc8e958e089329d0
Robbie Keehan <Rkeehan@Robbies-MacBook-Pro.local>
2018-04-29 20:41:19 -0500

    adding authentication and fixing seacrh functions

---
+++ realStuff/Fit/src/app/auth/auth.service.ts
@@ -0,0 +1,69 @@
+import { Account } from './../domain/models/Account';
+
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import * as jwt_decode from 'jwt-decode';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+import jwt from 'jsonwebtoken';
+
+
+export const TOKEN_NAME = 'jwt_token';
+
+@Injectable()
+export class AuthService {
+
+  private endPoint = 'http://192.168.99.100:3000/';
+  private httpOptions  = {
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json',
+      'Authorization' : 'rkeehan'
+    })
+  };
+
+  constructor(protected httpClient: HttpClient) { }
+
+  getToken(): string {
+    return localStorage.getItem(TOKEN_NAME);
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem(TOKEN_NAME, token);
+  }
+
+  getTokenExpirationDate(token: string): Date {
+    const decoded = jwt.decode(token);
+
+    if (decoded.exp === undefined) { return null; }
+
+    const date = new Date(0);
+    date.setUTCSeconds(decoded.exp);
+    return date;
+  }
+
+  isTokenExpired(token?: string): boolean {
+    if (!token) {token = this.getToken(); }
+    if (!token) {return true; }
+
+    const date = this.getTokenExpirationDate(token);
+    if (date === undefined) {return false; }
+    return !(date.valueOf() > new Date().valueOf());
+  }
+
+  public login(user: string, pass: string): Observable<Account> {
+    // tslint:disable-next-line:prefer-const
+    let data1 = {'username' : user, 'password' : pass};
+    const data = JSON.stringify(data1);
+    return this.httpClient.post(`${this.endPoint}signIn`, data, this.httpOptions).pipe(
+      catchError(this.handleException)
+    );
+  }
+
+  protected handleException(exception: any) {
+    // tslint:disable-next-line:prefer-const
+    let message = `${exception.status} : ${exception.statusText}\r\n${exception.message}`;
+    alert(message);
+    return Observable.throw(exception);
+  }
+}
