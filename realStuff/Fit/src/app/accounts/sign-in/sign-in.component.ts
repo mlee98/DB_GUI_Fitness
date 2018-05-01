48ddb55763327b8c1782abcfd69bff31d4e89ca9
Robbie Keehan <Rkeehan@Robbies-MacBook-Pro.local>
2018-04-30 23:52:53 -0500

    auth2

--- realStuff/Fit/src/app/accounts/sign-in/sign-in.component.ts
+++ realStuff/Fit/src/app/accounts/sign-in/sign-in.component.ts
@@ -35,7 +35,8 @@ export class SignInComponent implements OnInit {
   public goodLogin() {
     this.auth.login(this.username, this.pass).subscribe(data => {
        console.log(data);
-       const token = data.HttpHeaders.authorization;
+       console.log(data.headers);
+       const token = data.headers;
        this.auth.setToken(token);
         if (data.id === -1) {
           this.goodLog = 1;
